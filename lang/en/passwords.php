625bde05e339fbd0ad9ef4b07bcbfb52f7844215
DogukanMuhtar <148693057+DogukanMuhtar@users.noreply.github.com>
2026-07-20 09:29:07 +0300

    Add mail templates and switch locale to Turkish
    Change app locale to 'tr' and translate password lines into Turkish. Add a Turkish translation JSON for a mail action helper. Add vendor mail templates (HTML and text) and a default CSS theme under resources/views/vendor/mail, including layout, header, footer, button, panel, subcopy, table and message views.

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Şifreniz başarıyla sıfırlandı.',
+    'sent' => 'Şifre sıfırlama bağlantısı e-posta adresinize gönderildi.',
+    'throttled' => 'Lütfen tekrar denemeden önce biraz bekleyin.',
+    'token' => 'Bu şifre sıfırlama belirteci geçersizdir.',
+    'user' => "Bu e-posta adresiyle kayıtlı bir kullanıcı bulunamadı.",
 
 ];
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ url('assets/images/logo.png') }}"
+     class="logo"
+     alt="GBB Bilişim">
+
+
+</a>
+</td>
+</tr>
